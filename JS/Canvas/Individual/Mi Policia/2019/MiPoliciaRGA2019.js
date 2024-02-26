2f2b84df0602be04beb98c60291d1517aaef4cec
ssccdmx <157829465+ssccdmx@users.noreply.github.com>
2024-02-26 11:25:45 -0600

    Add files via upload

---
+++ JS/Canvas/Individual/Mi Policia/2019/MiPoliciaRGA2019.js
@@ -0,0 +1,36 @@
+var data1 = {
+  labels: ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"],
+  datasets: [
+    {
+      label: "Final",
+      fillColor: "rgba(151,187,205,0.5)",
+      strokeColor: "rgba(151,187,205,0.8)",
+      highlightFill: "rgba(151,187,205,0.75)",
+      highlightStroke: "rgba(151,187,205,1)",
+      data: [2.7,2.8,2.5,2.9,3.2,2.7,2.4,1.9,4.5,4.3,4.2,3.7]
+
+    }
+  ]
+};
+
+var ctx = document.getElementById("Promedio").getContext("2d");
+var myBarChart = new Chart(ctx).Bar(data1);
+
+var data2 = {
+  labels: ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"],
+  datasets: [
+    {
+      label: "Promedio",
+      fillColor: "rgba(220,220,220,0.5)",
+      strokeColor: "rgba(220,220,220,0.8)",
+      highlightFill: "rgba(220,220,220,0.75)",
+      highlightStroke: "rgba(220,220,220,1)",
+
+      data: [25,39,18,292,193,22,45,182,188,226,8,34]
+
+    }
+  ]
+};
+
+var ctx = document.getElementById("Productividad").getContext("2d");
+var myBarChart = new Chart(ctx).Bar(data2);
---
+++ JS/Canvas/Individual/Mi Policia/2019/MiPoliciaRVR2019.js
@@ -0,0 +1,36 @@
+var data1 = {
+  labels: ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"],
+  datasets: [
+    {
+      label: "Final",
+      fillColor: "rgba(151,187,205,0.5)",
+      strokeColor: "rgba(151,187,205,0.8)",
+      highlightFill: "rgba(151,187,205,0.75)",
+      highlightStroke: "rgba(151,187,205,1)",
+      data: [0.0,2.3,0.9,1.7,1.8,1.7,2.3,1.8,3.4,0.0,0.0,2.9]
+
+    }
+  ]
+};
+
+var ctx = document.getElementById("Promedio").getContext("2d");
+var myBarChart = new Chart(ctx).Bar(data1);
+
+var data2 = {
+  labels: ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"],
+  datasets: [
+    {
+      label: "Promedio",
+      fillColor: "rgba(220,220,220,0.5)",
+      strokeColor: "rgba(220,220,220,0.8)",
+      highlightFill: "rgba(220,220,220,0.75)",
+      highlightStroke: "rgba(220,220,220,1)",
+
+      data: [0,5,9,116,47,23,56,21,47,0,0,17]
+
+    }
+  ]
+};
+
+var ctx = document.getElementById("Productividad").getContext("2d");
+var myBarChart = new Chart(ctx).Bar(data2);
