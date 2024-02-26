4d958cd81f2873b43d8e02ce233ccd10e2a51126
ssccdmx <157829465+ssccdmx@users.noreply.github.com>
2024-02-26 11:31:27 -0600

    Add files via upload

---
+++ JS/Canvas/Individual/WhatsApp/2019/WhatsAppCAMY2019.js
@@ -0,0 +1,36 @@
+var data1 = {
+  labels: ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"],
+  datasets: [
+    {
+      label: "Final",
+      fillColor: "rgba(151,187,205,0.5)",
+      strokeColor: "rgba(151,187,205,0.8)",
+      highlightFill: "rgba(151,187,205,0.75)",
+      highlightStroke: "rgba(151,187,205,1)",
+      data: [0,1.3,0.9,0,4.2,3.6,6.8,3.7,0,0,0,0]
+
+    }
+  ]
+};
+
+var ctx = document.getElementById("Promedio").getContext("2d");
+var myBarChart = new Chart(ctx).Bar(data1);
+
+var data2 = {
+  labels: ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"],
+  datasets: [
+    {
+      label: "Promedio",
+      fillColor: "rgba(220,220,220,0.5)",
+      strokeColor: "rgba(220,220,220,0.8)",
+      highlightFill: "rgba(220,220,220,0.75)",
+      highlightStroke: "rgba(220,220,220,1)",
+      data: [0,1,4,0,2,4,3,2,0,0,0,0]
+
+
+    }
+  ]
+};
+
+var ctx = document.getElementById("Productividad").getContext("2d");
+var myBarChart = new Chart(ctx).Bar(data2);
